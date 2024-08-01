e4f3d4b8603523297a699f4331e18849eb1da550
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2024-08-01 17:13:01 +0200

    feat: add sample unit tests

---
+++ tests/lib2/sampleClassTests.C
@@ -0,0 +1,34 @@
+#include "ListOps.H"
+#include "catch2/catch_all.hpp"
+#include "catch2/catch_message.hpp"
+#include "catch2/catch_test_macros.hpp"
+#include "argList.H"
+#include "subCycleTime.H"
+#include "sampleClass.H"
+
+using namespace Foam;
+
+extern Time* timePtr;
+extern argList* argsPtr;
+
+TEST_CASE
+(
+    "`operator()()` must return what its docs says it will return",
+    "[cavity][serial]"
+) {
+    int X = GENERATE(1,5);
+    sampleClass sc(X);
+    auto expected = Foam::pow(12.0, -2.5*X); // 12 is a static private 
+    REQUIRE(sc() == expected);
+}
+
+TEST_CASE
+(
+    "incrementing a sampleClass object must be reflected on the x member",
+    "[cavity][serial]"
+) {
+    int X = GENERATE(1,5);
+    sampleClass sc(X);
+    ++sc;
+    REQUIRE(sc.x() == X+1);
+}
