24d24c897504ad322cb29b3e414f942db8ce4873
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2024-11-03 11:50:40 +0100

    fix: minor fixes for testing samples

--- src/lib1/sample.C
+++ src/lib1/sample.C
@@ -1,6 +1,13 @@
 
-/// @output_section author:elwardi
 namespace Foam {
+
+/**
+ * Test concept.
+ */
+template<class T>
+concept UIElement = requires {
+    { T::init() } -> std::convertible_to<bool>;
+};
 
 /// Global typedef using using. Not a typo :wave: :joy:
 using coolType1 = int;
--- src/lib2/functions.C
+++ src/lib2/functions.C
@@ -2,7 +2,6 @@
 
 namespace Foam {
     /// @brief Substract two sampleClass objects; delegates to `x` members
-    /// @output-section authors:elwardi
     sampleClass operator-(sampleClass& a, sampleClass& b) {
         return sampleClass(a.x() - b.x());
     }
